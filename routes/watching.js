9c175a26e608bcd00526cf63969088c923804406
tunang <nguyentuan20042207@gmail.com>
2024-08-01 01:04:15 +0700

    update watching route

--- routes/watching.js
+++ routes/watching.js
@@ -59,6 +59,91 @@ router.put("/", verifyToken, async (req, res) => {
       res.status(500).json({ success: false, message: "Internel server error" });
     }
   });
+
+
+//@route PUT api/watching/add
+//@desc push 1 watching films into watching films models
+//@access Private
+
+router.put("/add", verifyToken, async (req, res) => {
+  const { films } = req.body;
+
+  try {
+    const watchingFilmsListUpdateCondition = { userId: req.userId };
+
+    // Retrieve the user's favorite films list
+    const userWatchingFilms = await watchingFilms.findOne(
+      watchingFilmsListUpdateCondition
+    );
+
+    // Check if any film in the list has the same ID
+    const isFilmAlreadyAdded = userWatchingFilms.watchingFilms.some(
+      (film) => film._id === films[0]._id
+    );
+
+    if (isFilmAlreadyAdded) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Film already added to watching" });
+    }
+
+    // Rest of your code to add the film to the list
+    // ...
+
+    let updateWatchingFilmsList = {
+      $push: {
+        watchingFilms: { $each: films },
+      },
+    };
+
+    const result = await watchingFilms.findOneAndUpdate(
+      watchingFilmsListUpdateCondition,
+      updateWatchingFilmsList
+    );
+
+    if (!result) {
+      return res
+        .status(401)
+        .json({ success: false, message: "watching films list not found" });
+    }
+
+    return res.json({
+      success: true,
+      message: "Happy learning",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internel server error" });
+  }
+});
+
+router.delete("/", verifyToken, async (req, res) => {
+  const { films } = req.body;
+  try {
+    const watchingFilmsListUpdateCondition = { userId: req.userId };
+
+    // Retrieve the user's favorite films list
+    const userWatchingFilms = await watchingFilms.findOne(
+      watchingFilmsListUpdateCondition
+    );
+
+    const newWatchingFilmsList = userWatchingFilms.watchingFilms.filter(film => film._id !== films[0]._id);
+
+    console.log(newWatchingFilmsList);
+
+    userWatchingFilms.watchingFilms = newWatchingFilmsList;
+
+    await userWatchingFilms.save();
+
+    return res.json({
+      success: true,
+      message: "Film was removed from favorites"
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internel server error" });
+  }
+});
   
 
 module.exports = router;
